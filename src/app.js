f294b460cbb38ba9c4af1a2831ea4f461802dc11
cdsouza <cedricdsouza98@gmail.com>
2024-04-20 19:13:35 +0530

    Add about text

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -26,7 +27,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'weather app',
-        name: 'x'
+        name: 'Cedric Dsouza'
     })
 })
 
@@ -103,6 +104,6 @@ app.get('*', (req, res) => {
 })
 
 //listens for requests
-app.listen(3000, () => {
-   console.log('server is up on port 3000') 
+app.listen(port, () => {
+   console.log('server is up on port ' + port) 
 })
